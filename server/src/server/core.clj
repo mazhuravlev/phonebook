48685b9df6306fe684c5b11b2de67a90bf627894
Mikhail Zhuravlev <m.zhuravlev61@gmail.com>
2018-12-14 21:13:48 +0300

    server

--- server/src/server/core.clj
+++ server/src/server/core.clj
@@ -1,5 +1,12 @@
 (ns server.core)
+(require '[org.httpkit.server :as http])
 
-(defn -main
-  []
-  (println "SERVER!"))
+(defn app [req]
+  (println req)
+  {:status  200
+   :headers {"Content-Type" "text/html;charset=utf-8"}
+   :body    (str (:uri req) " тут такого нет")})
+
+(defn -main [& args]
+  (http/run-server app {:port 8080})
+  (println "Server started on port 8080"))
